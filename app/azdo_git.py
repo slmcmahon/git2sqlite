a1aa2a121092d386711a87bb5ab5d45f8f86a0be
Stephen McMahon <smcmahon@slb.com>
2023-01-02 08:07:46 -0600

    Fixed bug with refreshing existing repos.

--- app/azdo_git.py
+++ app/azdo_git.py
@@ -21,7 +21,7 @@ class AZDOGit():
 
     def repos(self, filter=None):
         url = f'{self.base_url}/repositories?api-version=7.0'
-        response = self._get_data(url)
+        response = self.__get_data(url)
         data = response['value']
         if filter:
             return [r['name'] for r in data if filter.lower() in r['name'].lower()]
@@ -31,53 +31,53 @@ class AZDOGit():
     def commits(self, repo, date_from=None):
         hasMore = True
         skip = 0
-        url = self._get_url(repo, self.PAGE_SIZE, skip, date_from)
+        url = self.__get_url(repo, self.PAGE_SIZE, skip, date_from)
         while hasMore:
-            data = self._get_data(url)
+            data = self.__get_data(url)
             item_count = data['count']
             if item_count > 0:
                 for commit in data['value']:
-                    yield self._parse_commit_data(repo, commit)
+                    yield self.__parse_commit_data(repo, commit)
                 skip += item_count
-                url = self._get_url(repo, self.PAGE_SIZE, skip, date_from)
+                url = self.__get_url(repo, self.PAGE_SIZE, skip, date_from)
             else:
                 hasMore = False
 
     def changes(self, repo, commit_id):
         url = f'{self.base_url}/repositories/{repo}/commits/{commit_id}/changes'
-        data = self._get_data(url)
+        data = self.__get_data(url)
         return [
             Change(commit_id=cr['item']['commitId'], path=cr['item']
                    ['path'], change_type=cr['changeType'])
             for cr in data['changes']
             if cr['item']['gitObjectType'] == 'blob']
 
-    def _get_url(self, repo, pagesize, skip, since):
+    def __get_url(self, repo, pagesize, skip, since):
         url = f'{self.base_url}/repositories/{repo}/commits?api-version=7.0&$top={pagesize}&$skip={skip}&searchCriteria.showOldestCommitsFirst=true'
         if since:
             formatted_date = datetime.strftime(since, '%Y-%m-%dT%H:%M:%SZ')
             url = f'{url}&searchCriteria.fromDate={formatted_date}'
         return url
 
-    def _get_data(self, url):
+    def __get_data(self, url):
         response = requests.get(url, auth=HTTPBasicAuth('user', self.token))
         if response.status_code != 200:
             raise Exception(
                 f'Got Status Code {response.status_code}.  Message: {response.reason}')
         return json.loads(response.content.decode('utf-8'))
 
-    def _parse_commit_data(self, repo, ci):
+    def __parse_commit_data(self, repo, ci):
         id = ci['commitId']
         comment = ci['comment']
         author = ci['author']
         author_name = author['name']
         author_email = author['email']
-        author_date = self._clean_date_string(author['date'])
+        author_date = self.__clean_date_string(author['date'])
         committer = ci['committer']
         committer_name = committer['name']
         committer_email = committer['email']
-        committer_date = self._clean_date_string(committer['date'])
+        committer_date = self.__clean_date_string(committer['date'])
         return Commit(id, repo, comment, author_name, author_email, author_date, committer_name, committer_email, committer_date)
 
-    def _clean_date_string(self, date_string):
+    def __clean_date_string(self, date_string):
         return " ".join(date_string.split('T'))[:-1]
--- app/git_db.py
+++ app/git_db.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from app.azdo_git import Commit, Change
 from typing import collections
 
+
 COMMITS_TABLE = """create table if not exists commits (
     id text unique, 
     repo_name text, 
@@ -16,6 +17,7 @@ COMMITS_TABLE = """create table if not exists commits (
     committer_date datetime
 );
 """
+
 FILES_TABLE = """create table if not exists commit_files (
     id text, 
     path text, 
@@ -27,15 +29,18 @@ FILES_TABLE = """create table if not exists commit_files (
 );
 """
 
+
 INSERT_COMMIT = """insert into commits (
     id, repo_name, summary, author_name, author_email, author_date, 
     committer_name, committer_email, committer_date) 
 values (?, ?, ?, ?, ?, ?, ?, ?, ?)
 """
+
 INSERT_CHANGE = """insert into commit_files (
     id, path, change_type) 
 values (?, ?, ?)
 """
+
 EXISTING_REPOS = """
 select 
     repo_name, 
@@ -44,11 +49,13 @@ from commits
 group by repo_name 
 order by repo_name
 """
+
 REPO_LAST_MODIFIED = """
 select max(committer_date) 
 from commits 
 where repo_name = ?
 """
+
 
 DbRepo = namedtuple("DbRepo", "name last_commit")
 
@@ -59,7 +66,7 @@ class GitDB():
         self.__init_db()
 
     def add_commit(self, commit: Commit, changes: any):
-        conn = self._get_connection()
+        conn = self.__get_connection()
         csr = conn.cursor()
         csr.execute(INSERT_COMMIT,
                     (commit.commit_id, commit.repo,
@@ -74,28 +81,25 @@ class GitDB():
         conn.close()
 
     def add_change(self, change: Change):
-        conn = self._get_connection()
+        conn = self.__get_connection()
         csr = conn.cursor()
         csr.execute(INSERT_CHANGE,
                     (change.commit_id, change.path, change.change_type))
         conn.commit()
         conn.close()
 
-    def __add_seconds(dateString, s):
-        dt = datetime.strptime(dateString, '%Y-%m-%d %H:%M:%S')
-        return dt + timedelta(seconds=s)
-
     def existing_repos(self):
-        conn = self._get_connection()
+        conn = self.__get_connection()
         csr = conn.cursor()
         csr.execute(EXISTING_REPOS)
         rows = csr.fetchall()
-        repos = [DbRepo(row[0], self.__add_seconds(row[1], 1)) for row in rows]
+        repos = [DbRepo(name=row[0], last_commit=self.__add_seconds(
+            row[1], 1)) for row in rows]
         conn.close()
         return repos
 
     def get_last_modified(self, repo):
-        conn = self._get_connection()
+        conn = self.__get_connection()
         csr = conn.cursor()
         csr.execute(REPO_LAST_MODIFIED, (repo,))
         lmd = csr.fetchone()[0]
@@ -105,12 +109,16 @@ class GitDB():
         return self.__add_seconds(lmd, 1)
 
     def __init_db(self):
-        conn = self._get_connection()
+        conn = self.__get_connection()
         csr = conn.cursor()
         csr.execute(COMMITS_TABLE)
         csr.execute(FILES_TABLE)
         conn.commit()
         conn.close()
 
-    def _get_connection(self):
+    def __get_connection(self):
         return sqlite3.connect(self.db_path)
+
+    def __add_seconds(self, dateString, s):
+        dt = datetime.strptime(dateString, '%Y-%m-%d %H:%M:%S')
+        return dt + timedelta(seconds=s)
